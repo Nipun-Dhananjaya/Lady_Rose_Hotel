d36cb3ce89515c418af0e6d2bf4d72d389723b34
Nipun-Dhananjaya <nipundhananjaya967@gmail.com>
2025-11-22 11:56:23 +0530

    add dependencies and AppInitializer

--- src/main/java/AppInitializer.java
+++ src/main/java/AppInitializer.java
@@ -1,2 +1,17 @@
-public class AppInitializer {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.centerOnScreen();
+        stage.setTitle("Profile Selection");
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/profileSelectionForm.fxml"))));
+        stage.show();
+    }
 }
