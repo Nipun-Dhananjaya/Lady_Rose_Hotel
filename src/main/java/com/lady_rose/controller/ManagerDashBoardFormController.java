4f3ba4cb003764462a7ec88c8b40fcf39141fac8
Nipun-Dhananjaya <nipundhananjaya967@gmail.com>
2025-12-16 00:57:11 +0530

    create managerDashBoardForm.fxml, receptionistDashBoardForm.fxml and controllers

---
+++ src/main/java/com/lady_rose/controller/ManagerDashBoardFormController.java
@@ -0,0 +1,29 @@
+package com.lady_rose.controller;
+
+import javafx.event.ActionEvent;
+
+public class ManagerDashBoardFormController {
+    public void showEmployeeManageOnAction(ActionEvent actionEvent) {
+    }
+
+    public void showRoomManageOnAction(ActionEvent actionEvent) {
+    }
+
+    public void showSupplierManageOnAction(ActionEvent actionEvent) {
+    }
+
+    public void showOrderManageOnAction(ActionEvent actionEvent) {
+    }
+
+    public void loadHomeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void loadProfileSelectionOnAction(ActionEvent actionEvent) {
+    }
+
+    public void showItemManageOnAction(ActionEvent actionEvent) {
+    }
+
+    public void showSettingsFormOnAction(ActionEvent actionEvent) {
+    }
+}
